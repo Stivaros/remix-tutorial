4b49b133591b62e822192bde0bea1ea05f1055d1
Efstathios Stivaros <stath.stivaros@shopify.com>
2024-02-01 15:11:58 +0200

    build: use Link to prevent entire page reload

--- app/root.tsx
+++ app/root.tsx
@@ -8,6 +8,7 @@ export const links: LinksFunction = () => [
 
 import {
   Form,
+  Link,
   Links,
   LiveReload,
   Meta,
@@ -46,10 +47,10 @@ export default function App() {
           <nav>
             <ul>
               <li>
-                <a href={`/contacts/1`}>Your Name</a>
+                <Link to={`/contacts/1`}>Your Name</Link>
               </li>
               <li>
-                <a href={`/contacts/2`}>Your Friend</a>
+                <Link to={`/contacts/2`}>Your Friend</Link>
               </li>
             </ul>
           </nav>
